fcf3c1292a870a4c4d564bbc5f40d80408d80e36
tnemgarf89 <nikola.stamenkovic1989@gmail.com>
2026-04-19 15:34:54 +0200

    m14

--- main.js
+++ main.js
@@ -2,4 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
   const background = "./img/background.png";
 
   document.body.style.backgroundImage = `url('${background}')`;
+
+  const selectGlavno = document.getElementById("glavno-jelo");
+  const selectPice = document.getElementById("pice");
+  const selectGrickalice = document.getElementById("grickalice");
+
+  const verifyButton = document.getElementById("verify");
+
+  verifyButton.addEventListener("click", () => {
+    verifyOrder() ? alert("super") : alert("ne bas");
+  });
+
+  function verifyOrder() {
+    if (selectGlavno.value !== "glavno-hot-dog.png") {
+      return false;
+    }
+
+    if (selectPice.value !== "piće-limunada.png") {
+      return false;
+    }
+
+    if (selectGrickalice.value !== "grickalice-kokice.png") {
+      return false;
+    }
+
+    return true;
+  }
 });
